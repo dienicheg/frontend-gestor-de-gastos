cb60dff329f282405bc788371513cc1d571b3b01
Nicolás Chazarreta <dienicheg.13@gmail.com>
2023-10-04 06:27:54 -0300

    updated

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     children: [
       { path: 'register', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
-      { path: '**', redirectTo: 'register'}
+      { path: '**', redirectTo: 'login'}
     ]
   }
 
